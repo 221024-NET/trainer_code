68dd743b1bf062764b8ee4c52409e0a1254a5fcf
HawkinsR <91553090+HawkinsR@users.noreply.github.com>
2022-11-02 15:12:16 -0400

    Added Dictionary Tests (#8)
    Co-authored-by: KevinCornell <kevdcornell@gmail.com>

--- w2/CollectionsExamples/CollectionsExamples.App/Timer.cs
+++ w2/CollectionsExamples/CollectionsExamples.App/Timer.cs
@@ -7,7 +7,6 @@ namespace CollectionsExamples.App
         // Constructors
         public Timer(){}
 
-        // Methods
         public TimeSpan Run()
         {
 
@@ -15,8 +14,81 @@ namespace CollectionsExamples.App
             DateTime start = DateTime.Now;
 
             // some time intensive action
-            Demo tmp = new Demo();
-            tmp.getDifferences();
+            DictionaryTests tmp = new DictionaryTests();
+            //tmp.printSomething();
+
+            // stop the timer
+            DateTime stop = DateTime.Now;
+
+            TimeSpan ts = stop - start;
+
+            // return the value of the timer
+            return ts;
+        }
+
+        // Methods
+        public TimeSpan RunDictionary()
+        {
+
+            // start a timer
+            DateTime start = DateTime.Now;
+
+            // some time intensive action
+            DictionaryTests tmp = new DictionaryTests();
+            //tmp.printSomething();
+
+            // stop the timer
+            DateTime stop = DateTime.Now;
+
+            TimeSpan ts = stop - start;
+
+            // return the value of the timer
+            return ts;
+        }
+
+        public TimeSpan RunArray()
+        {
+
+            // start a timer
+            DateTime start = DateTime.Now;
+
+            // some time intensive action
+            string [,] arr = new string [3,2];
+            arr[0,0] = "animal";
+            arr[0,1] = "dog";
+            arr[1,0] = "canine";
+            arr[1,1] = "cat";
+            arr[2,0] = "herbivore";
+            arr[2,1] = "hamster";
+            Console.WriteLine("------Initializing Array------");
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine("Key: {0}, Value: {1}",arr[i,0], arr[i,1]);
+            }
+            Console.WriteLine("-----------------------------------");
+
+            arr[1,1] = "dog";
+
+            Console.WriteLine("-------Modifying: Cat to Dog--------");
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine("Key: {0}, Value: {1}",arr[i,0], arr[i,1]);
+            }
+            Console.WriteLine("------------------------------------");
+
+            string [,] newArr = new string [2,2];
+
+            for (int i = 0; i < 2; i++) {
+                newArr[i, 0] = arr[i+1, 0];
+                newArr[i, 1] = arr[i+1, 1];
+            }
+
+            Console.WriteLine("------Removing: Animal--------------");
+            for (int i = 0; i < 2; i++) {
+                Console.WriteLine("Key: {0}, Value: {1}",newArr[i,0], newArr[i,1]);
+            }
+            Console.WriteLine("------------------------------------");
+            
+
+            
 
             // stop the timer
             DateTime stop = DateTime.Now;
