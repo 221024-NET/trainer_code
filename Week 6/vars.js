80059757894d15bb9de68330a4c4d21af7ce807f
ajaysingala <ajay.singala@gmail.com>
2022-11-28 16:30:26 -0500

    week 6 - javascript

---
+++ Week 6/vars.js
@@ -0,0 +1,124 @@
+// let username = 'John';
+// let age = 25;
+// let message = 'Hi there!'
+
+// alert(username);
+// alert(age);
+// alert(message);
+
+// alert(10 + age);
+
+// let num = '25';
+// alert(10 + num);
+
+// let $ = 1;
+// let _ = 2;
+// alert( $ + _);
+
+// Invalid:
+//let 1abc = 10;
+//let some-name = 'Mary';
+
+// let name = 'John';
+// let Name = 'Mary';
+// alert(name);
+// alert(Name);
+
+// Cannot use Keywords as variable names:
+//let let = 50;
+//let return = 'Hello';
+
+// Constant.
+// const status = 'Pending';
+// alert(status);
+// status = 'Approved';
+
+// const COLOR_RED = '#F00';
+// const COLOR_GREEN = '#0F0';
+// const COLOR_BLUE = '#00F';
+// let currentColor = COLOR_RED;
+// alert(currentColor);
+
+// Data Types:
+// let message = 'Hello';
+// alert(message);
+// message = 123456;
+// alert(message);
+
+// let n= 123;
+// alert(n);
+// n = 12.345;
+// alert(n);
+
+//alert(1/0);
+//alert(Infinity);
+
+// alert("this is not a number" / 2);		// NaN.
+// alert(NaN + 5);							// NaN.
+// alert(3 * NaN);							// NaN.
+
+// Strings:
+// Back-tick.
+// let name = 'John';
+// alert(`Hello, ${name}`);
+// //alert(`Hello, ${noname}`);		// Error.
+// alert(`The result of 1 + 2 is ${1 + 2}`);
+
+// Boolean:
+// let isApproved = true;
+// let isValid = false;
+// let isGreater =  1 > 4;
+// alert(isGreater);
+
+// The "null" value:
+// let age = null;
+// alert(age);
+// age = 20;
+// alert(age);
+// age = null;
+// alert(age * 20);
+
+//The "undefined" value:
+// let age = undefined;
+// alert(age);
+// age = 20;
+// alert(age);
+// age = undefined;
+// alert(age * 20);	// NaN.
+// let name;
+// alert(name);		// Undefined.
+
+// typeof operator:
+// alert(typeof 10);
+// alert(typeof "John");
+// alert(typeof true);
+// alert(typeof NaN);			// number.
+// alert(typeof null);			// object.
+// alert(typeof undefined);	// undefined.
+
+// Comparisons:
+// alert(2 > 1);		// true
+// alert( 2 != 1);		// true
+// alert( 2 == 1);		// false
+// alert( 2 == "2");	// true
+// alert( 2 === "2");	// ??
+
+// String comparison:
+// alert('Z' > 'A');			// true.
+// alert('Glow' > 'Glee');		// true.
+// alert('Glow' > 'glee');		// false.
+
+// alert(true == 1);		// true.
+// alert( false == 0);		// true.
+
+// alert(true === 1);		// false.
+// alert( false === 0);	// false.
+
+// alert(null == undefined);	// true.
+// alert(null === undefined);	// false.
+
+alert(null > 0);	// false.
+alert(null == 0);	// false.
+alert(null >= 0);	// true.
+
+
