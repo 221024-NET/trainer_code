52ca56ebee855d570d4307ad31f824fa23269b54
ajaysingala <ajay.singala@gmail.com>
2022-12-07 11:23:24 -0500

    week7: angular services

---
+++ Week7/Angular/inout/src/app/components/attr-directives/attr-directives.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/classes/user';
+
+@Component({
+  selector: 'app-attr-directives',
+  templateUrl: './attr-directives.component.html',
+  styleUrls: ['./attr-directives.component.css']
+})
+export class AttrDirectivesComponent implements OnInit {
+  users: User[] = [ 
+    { 
+       "userId": 1, 
+       "userName": 'User1' 
+    }, 
+    { 
+       "userId": 2, 
+       "userName": 'User2' 
+    }, 
+    { 
+       "userId": 3, 
+       "userName": 'User3' 
+    } 
+  ];
+
+  serverID: number = 10;  
+  serverStatus: string = 'Offline';
+  constructor() { 
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';  
+  }
+
+  ngOnInit(): void {
+  }
+
+  getServerStatus() {  
+    return this.serverStatus;  
+  }  
+  getColor() {  
+    return this.serverStatus === 'Online' ? 'green' : 'red';  
+  }
+}
