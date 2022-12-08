bc8a7847b0abd4a1b5d05c5d6ec551f362b807e2
ajaysingala <ajay.singala@gmail.com>
2022-12-08 11:33:54 -0500

    Week 7: Angular Forms and Validations

---
+++ Week7/Angular/forms-app/src/app/components/hero-form/hero-form.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClientBackendService } from 'angular-in-memory-web-api';
+import { Hero } from 'src/app/classes/hero';
+import { User } from 'src/app/classes/user';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent implements OnInit {
+
+  powers = ["Really Smart", "Super Flexible", "Super Hot", "Weather Changer"];
+  model = new Hero(18, "Dr. IQ", "", "Chuck Overstreet");
+  submitted = false;
+
+  user = new User("Ajay");
+  cityname = "Reston";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.user.firstname)
+    console.log(this.cityname)
+  }
+
+  newHero() {
+    this.model = new Hero(42, "", "");
+    //this.model = new User("Ajay");
+  }
+
+}
